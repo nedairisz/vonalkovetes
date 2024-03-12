ea22dcaf3439632320baec410a0f8676e46c736f
nedairisz <irisz.neda@gmail.com>
2024-03-12 08:49:39 +0100

    korbemegy

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+import Feladatok
+
+
+
+# Create your objects here.
+#ev3 = EV3Brick()
+
+# főprogram
+oraiMunka=Feladatok.Feladatok()
+#elöző órai munkák megoldása
+# oraiMunka.elsoFeladat()
+# oraiMunka.masodikFeladat()
+# oraiMunka.harmadikFeladat()
+# oraiMunka.negyedikFeladat()
+#oraiMunka.mozog1()
+#oraiMunka.mozog2()
+# fordulások
+oraiMunka.korbefordul1()
+oraiMunka.korbefordul2()
+oraiMunka.korbefordul3()
+# Hangkezelés, téglavilágítás
+# oraiMunka.koszon()
+oraiMunka.vonalkovet()
+
+# Write your program here.
+#ev3.speaker.beep()
+oraiMunka.csipog()
